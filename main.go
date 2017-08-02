f9a75b892cebda8c9c7f1c857ae3bcb561597e6a
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-08-02 16:57:19 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+  "os"
+  "fmt"
+  "flag"
+  "dev/hello"
+)
+
+// Global Flags
+var force bool
+
+func do() {
+  doFlags := flag.NewFlagSet("do", flag.ExitOnError)
+  doFlags.BoolVar(&force, "f", false, "force")
+  doFlags.Parse(flag.Args()[1:])
+  if doFlags.Parsed() {
+    if len(doFlags.Args()) == 0 {
+      fmt.Println("Must provide something to do!")
+      os.Exit(1)
+    }
+	  fmt.Printf("Run Command: %q\n", doFlags.Args())
+  }
+}
+
+func parseFlags() {
+  // Global Flags
+  flag.BoolVar(&force, "f", false, "force")
+  flag.Parse()
+
+  if len(flag.Args()) <= 0 {
+    hello.World()
+    os.Exit(1)
+  }
+
+  switch flag.Args()[0] {
+  case "do":
+    do()
+  case "hello":
+    hello.World()
+  default:
+  }
+}
+
+func main() {
+  fmt.Println("**  KCLI  **")
+  parseFlags()
+}
