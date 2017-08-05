b2d8384bb46bb410fc1eb0d0d085d2109b230b31
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-08-05 01:11:45 -0400

    update config

--- configure.go
+++ configure.go
@@ -1,67 +1,65 @@
 package main
 
-import(
-  "os"
-  "fmt"
-  "io/ioutil"
-  "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	gt "github.com/seemywingz/gtills"
 )
 
-import gt "github.com/seemywingz/gtills"
+const configDir = ".myApp"
+const configFileName = "config"
 
-var homeDir,configFile string
+var homeDir, configFile string
 
-const configFileName = "/.myApp/config"
-
-type jsonConfig struct{
-  Fname string `json:"fname"`
-  Lname string `json:"lname"`
-  Email string `json:"email"`
+type jsonConfig struct {
+	Fname string `json:"fname"`
+	Lname string `json:"lname"`
+	Email string `json:"email"`
 }
+
 var config jsonConfig
 
+// Configure : Gather User Informaton and save it to config file
 func Configure() {
 
-  fmt.Println("📝  ",configFile)
-  gt.GetInput(&config.Fname,"First Name:")
-  gt.GetInput(&config.Lname,"Last Name:")
-  gt.GetInput(&config.Email,"Email:")
+	fmt.Println("📝  ", configFile)
+	gt.SetFromInput(&config.Fname, "\nFirst Name: ")
+	gt.SetFromInput(&config.Lname, " Last Name: ")
+	gt.SetFromInput(&config.Email, "     Email: ")
 
-  data, err := json.Marshal(config)
-  if err != nil {
-    fmt.Println("❌  Error converting json",err)
-    os.Exit(1)
-  }
+	data, jsoEerr := json.Marshal(config)
+	gt.E(jsoEerr, "Error Parsing Json:")
 
-  err = ioutil.WriteFile(configFile, data, 0644)
-  if err != nil {
-    fmt.Println("❌  Error Saving Config File",err)
-    os.Exit(2)
-  }
+	err := ioutil.WriteFile(configFile, data, 0644)
+	gt.E(err, "Error Saving Config File:")
+	fmt.Println("\n✨  Configuration File Saved Successfully")
 }
 
+// GetConfig : Check to see if there is a config file, if not create one
 func GetConfig() {
-  homeDir := gt.GetHomeDir()
-  if homeDir == "" {
-    os.Exit(1)
-  }
-  configFile = homeDir + configFileName
-  if _, err := os.Stat(configFile); os.IsNotExist(err) {
-    fmt.Println("❗ CONFIG NOT FOUND")
-    var ans string
-    gt.GetInput(&ans,"⚙  Want to Create one now? (Y/n)")
-    if ans == "y" || ans == ""{
-      Configure()
-    }else{
-      fmt.Println("⏩  Skipping Configuration File Creation")
-      os.Exit(10)
-    }
-  }else{ // config exists
-    jsonFile, err := ioutil.ReadFile(configFile)
-    if err != nil {
-      fmt.Println("❌  Error Reading Config File",err)
-      os.Exit(2)
-    }
-    json.Unmarshal(jsonFile, &config)
-  }
+	homeDir := gt.GetHomeDir()
+	if homeDir == "" {
+		os.Exit(1)
+	}
+	configFile = filepath.Join(homeDir, configDir, configFileName)
+	if _, err := os.Stat(configFile); os.IsNotExist(err) {
+		fmt.Println("❗ CONFIG NOT FOUND")
+		ans := gt.GetInput("⚙  Want to Create one now? (Y/n): ")
+		if ans == "y" || ans == "" {
+			err := os.MkdirAll(filepath.Join(homeDir, configDir), os.ModePerm)
+			gt.E(err, "Error Creating Config Directory:")
+			Configure()
+		} else {
+			fmt.Println("⏩  Skipping Configuration File Creation")
+			os.Exit(10)
+		}
+	} else { // config exists
+		jsonFile, err := ioutil.ReadFile(configFile)
+		gt.E(err, "Error Reading Config File:")
+		json.Unmarshal(jsonFile, &config)
+	}
 }
--- main.go
+++ main.go
@@ -1,39 +1,39 @@
 package main
 
-import(
-  "github.com/voxelbrain/goptions"
+import (
+	"github.com/voxelbrain/goptions"
 )
 
 func main() {
-  options := struct {
-		Help  goptions.Help `goptions:"-h, --help, description='Show this help'"`
-		Tail  goptions.Help `goptions:"-t, --tail, description='Follow the logs'"`
+	options := struct {
+		Help goptions.Help `goptions:"-h, --help, description='Show this help'"`
+		Tail goptions.Help `goptions:"-t, --tail, description='Follow the logs'"`
 
 		goptions.Verbs
-		Request struct {
-			Name bool   `goptions:"-n, --name, description='Name of Instance'"`
-		} `goptions:"request"`
+		// Request struct {
+		// 	Name bool   `goptions:"-n, --name, description='Name of Instance'"`
+		// } `goptions:"request"`
 
-    Status struct {
+		Status struct {
 		} `goptions:"status"`
 
-    Config struct {
-    } `goptions:"config"`
-
+		Config struct {
+			Name string `goptions:"-n, --name, description='Set Name in config file'"`
+		} `goptions:"config"`
 	}{ // Default values go here
-		// Force: true,
+	// Force: true,
 	}
 
-  goptions.ParseAndFail(&options)
-  GetConfig()
+	goptions.ParseAndFail(&options)
+	GetConfig()
 
-  switch options.Verbs {
-  case "status":
-    Status()
-  case "config":
-    Configure()
-  default:
-    // goptions.PrintHelp()
-  }
+	switch options.Verbs {
+	case "status":
+		Status()
+	case "config":
+		Configure()
+	default:
+		// goptions.PrintHelp()
+	}
 
 }
