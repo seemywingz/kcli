b1d1bdf781a0957ba23a9fdac9c4d0e29cc87032
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-08-05 22:44:33 -0400

    update send request

--- status.go
+++ status.go
@@ -6,11 +6,16 @@ import (
 	gt "github.com/seemywingz/gtills"
 )
 
-var jenkinsURL = "http://prod2.jenkins.lifion.oneadp.com/view/DITC/view/All/job/DitC_Lookup/"
+var jenkinsURL = "http://prod2.jenkins.lifion.oneadp.com/view/DITC/view/All/job/DitC_Lookup"
+
+func GetJenkinsBuildNumber() {
+	res := gt.SendRequest("GET", jenkinsURL, "")
+	println(res)
+}
 
 // Status : get current ditc status
 func Status() {
 	fmt.Println("Getting Ditc Status")
-	res := gt.SendRequest("GET", jenkinsURL)
+	res := gt.SendRequest("GET", jenkinsURL, "")
 	fmt.Println(res)
 }
